e406d4ee62dafeabc9d87d248caf283ba6542a97
lucocozz <lucocozz@student.42.fr>
2023-10-31 14:48:14 +0100

    adding useful defines

--- includes/matt-daemon.hpp
+++ includes/matt-daemon.hpp
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:49:39 by lucocozz          #+#    #+#             */
-/*   Updated: 2023/10/31 14:18:18 by lucocozz         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:44:46 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 #include <iostream>
 #include <exception>
+#include <string>
+#include <unistd.h>
+#include <signal.h>
 
 #include "class/tintin_reporter.hpp"
 
 #define PORT 4242
+#define LOG_FILE "/var/log/matt_daemon.log"
+#define LOCK_FILE "/var/lock/matt_daemon.lock"
+#define MAX_CLIENT 3
--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 13:48:57 by lucocozz          #+#    #+#             */
-/*   Updated: 2023/10/31 13:58:31 by lucocozz         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:47:55 by lucocozz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 int main()
 {
+	// if (getuid() != 0) {
+	// 	std::cerr << "You must be root to run this program." << std::endl;
+	// 	return (EXIT_FAILURE);
+	// }
 	return (EXIT_SUCCESS);
 }
