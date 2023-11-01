bdc334e9ef12f76ff31f73a104cd09df43014a2c
Nnevalti <deschamps.valentin.1998@gmail.com>
2023-11-01 15:27:20 +0100

    Added log for lock file

--- sources/lock.cpp
+++ sources/lock.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lock.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lucocozz <lucocozz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vdescham <vdescham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:17:19 by vdescham          #+#    #+#             */
-/*   Updated: 2023/10/31 18:27:18 by lucocozz         ###   ########.fr       */
+/*   Updated: 2023/11/01 15:18:39 by vdescham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 bool lock_file(void) {
 	g_global.fd_lock = open(LOCK_FILE, O_CREAT | O_RDWR, 644);
 	if (flock(g_global.fd_lock, LOCK_EX | LOCK_NB) < 0) {
-		// if (errno == EWOULDBLOCK)
-			std::cerr << "Error: " << LOCK_FILE << " is locked." << std::endl;
+		g_global.logger->logError("File already locked.");
+		std::cerr << "Can't open " << LOCK_FILE << std::endl;
 		return false;
 	}
 	return true;
